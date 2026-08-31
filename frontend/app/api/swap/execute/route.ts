03df841935e526761dc2ea0769485e50236502d2
TusharSin810 <tsinghal666@gmail.com>
2026-08-31 13:44:45 +0530

    Swap Route Completed

--- frontend/app/api/swap/execute/route.ts
+++ frontend/app/api/swap/execute/route.ts
@@ -1,3 +1,7 @@
+import prismaClient from "@/app/db";
+import { authConfig } from "@/app/lib/auth";
+import { Keypair, VersionedTransaction } from "@solana/web3.js";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest){
@@ -9,11 +13,46 @@ export async function POST(req: NextRequest){
         }
     } = await req.json();
 
+    if(data.order.transaction === ""){
+        return(NextResponse.json({
+            message:"Transaction Object Not Created"
+        }))
+    }
+
+    const session = await getServerSession(authConfig);
+    if(!session?.user){
+        return(
+            NextResponse.json({
+                message:"You Are Not Logged in"
+            },{
+                status: 401
+            })
+        )
+    }
+
     if(!data.order.requestId){
         return NextResponse.json({
             message: "Error Fetching Quote From Chain"
         })
     }
+
+    const solWallet = await prismaClient.solWallet.findFirst({
+        where:{
+            userId: session.user.uid
+        }
+    })
+    if(!solWallet){
+        return(
+            NextResponse.json({
+                message: "Error in Creating Users SolWallet"
+            })
+        )
+    }
+
+    const transactionBuf = Buffer.from(data.order.transaction, 'base64');
+    var transaction = VersionedTransaction.deserialize(transactionBuf);
+    const privateKey = getPrivateKey(solWallet.privateKey);
+    const signedTransaction = transaction.sign([privateKey])
 
     const options = {
         method: 'POST',
@@ -21,15 +60,25 @@ export async function POST(req: NextRequest){
         'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-        signedTransaction: '<string>',
+        signedTransaction: signedTransaction,
         requestId: data.order.requestId,
         })
     };
 
-    fetch('https://api.jup.ag/swap/v2/execute', options)
-        .then(res => res.json())
-        .then(res => console.log(res))
-        .catch(err => console.error(err));
+    const response = await fetch('https://api.jup.ag/swap/v2/execute', options)
 
+    const dataResponse = await response.json();
+    return(
+        NextResponse.json(dataResponse,{
+            status: response.status
+        })
+    )
+}
 
+function getPrivateKey(privateKey: string){
+    const arr = privateKey.split(",").map(x => Number(x));
+    const privateKeyUint = Uint8Array.from(arr);
+    const keyPair = Keypair.fromSecretKey(privateKeyUint);
+
+    return keyPair;
 }
